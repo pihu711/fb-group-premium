2e51bb0447b0b4261d7a49b5fad8f530cd3b3b0d
pihu711 <harshuuuuccccc@gmail.com>
2025-09-05 22:07:52 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./pages/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {
-      colors: {
-        neonpink: '#ff2d7a',
-      },
-      dropShadow: {
-        glowpink: '0 0 25px rgba(255, 45, 122, 0.9)',
-        glowcyan: '0 0 25px rgba(34, 211, 238, 0.9)',
-      },
-      keyframes: {
-        flow: {
-          '0%': { backgroundPosition: '0% 50%' },
-          '50%': { backgroundPosition: '100% 50%' },
-          '100%': { backgroundPosition: '0% 50%' },
-        }
-      },
-      animation: {
-        flow: 'flow 6s ease infinite',
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
