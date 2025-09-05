01bbd7e2d8d5a58afea01757609fa7ff34fb6fcc
pihu711 <harshuuuuccccc@gmail.com>
2025-09-05 22:00:27 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,5 @@
 import '@/styles/globals.css'
 
-export default function MyApp({ Component, pageProps }) {
-  return (
-    <div className="font-sans bg-black text-white">
-      <Component {...pageProps} />
-    </div>
-  )
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
 }
