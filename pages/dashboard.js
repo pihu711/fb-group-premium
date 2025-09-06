a5198d6507dd6e7098e16734ec3f27e5e6124497
pihu711 <harshuuuuccccc@gmail.com>
2025-09-06 22:28:14 +0530

    Delete pages/dashboard.js

--- pages/dashboard.js
+++
@@ -1,8 +0,0 @@
-export default function Dashboard() {
-  return (
-    <div className="dashboard">
-      <h1 className="dashTitle">🔥 HARSHU ❤️ PIHU Forever 🔥</h1>
-      <p className="tagline">Made with Love 💖</p>
-    </div>
-  );
-}
